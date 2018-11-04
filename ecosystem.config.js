56de9be06e4b37fbad039cb190ad2feede396f18
A. David Albertson <adavida@mac.com>
2018-11-04 10:45:42 -0700

    setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+      name: 'jess',
+      script: './backend/index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ec2-user',
+        host: 'ec2-34-216-180-83.us-west-2.compute.amazonaws.com',
+        key: '~/.ssh/aws-jess.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:adavidalbertson/jess.git',
+        path: '/home/ec2-user/jess',
+        'post-deploy': 'cd ./frontend && npm install && node run build'
+            + ' && cd ../backend && npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
