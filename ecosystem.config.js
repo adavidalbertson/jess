a43725d756f2fbd4acf431639a4d85848008fee4
A. David Albertson <adavida@mac.com>
2018-11-04 10:53:55 -0700

    directory bouncing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-    apps: [{
-      name: 'jess',
-      script: './backend/index.js'
-    }],
+    apps: [
+        {
+            name: "jess",
+            script: "./backend/index.js"
+        }
+    ],
     deploy: {
-      production: {
-        user: 'ec2-user',
-        host: 'ec2-34-216-180-83.us-west-2.compute.amazonaws.com',
-        key: '~/.ssh/aws-jess.pem',
-        ref: 'origin/master',
-        repo: 'git@github.com:adavidalbertson/jess.git',
-        path: '/home/ec2-user/jess',
-        'post-deploy': 'cd ./frontend && npm install && npm run build'
-            + ' && cd ../backend && npm install && pm2 startOrRestart ecosystem.config.js'
-      }
+        production: {
+            user: "ec2-user",
+            host: "ec2-34-216-180-83.us-west-2.compute.amazonaws.com",
+            key: "~/.ssh/aws-jess.pem",
+            ref: "origin/master",
+            repo: "git@github.com:adavidalbertson/jess.git",
+            path: "/home/ec2-user/jess",
+            "post-deploy":
+                "cd ./frontend && npm install && npm run build" +
+                " && cd ../backend && npm install" +
+                " && pm2 startOrRestart ../ecosystem.config.js"
+        }
     }
-  }
+};
