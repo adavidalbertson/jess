2cbbf3db7af355cd549162d0b4b54a35975ac949
A. David Albertson <adavida@mac.com>
2019-10-19 13:21:24 -0600

    Serve static files from node and set up scripts for EB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http).of('app1');
+const path = require('path');
 require('local-include-js');
 
 const defaultHandlers = require('react-redux-socket/server/handlers/');
@@ -12,6 +14,12 @@ ioActionHandler(io)
     .plugins(include('./handlers/GameState.js').log(console.log))
     // .onDisconnect(socketHandler.handleDisconnect);
 
+app.use(express.static(path.join(__dirname, '../frontend/dist/')));
+
+app.get('/', (req, res) => res.send('You got a response!'));
+
+const PORT = process.env.HTTP_PORT || 4040;
+
 http.listen(4040, function () {
-    console.log('listening on *:4040');
+    console.log(`listening on *:${PORT}`);
 });
